6a888ee1854069a1a0dfa1815afe5b7191b9987f
EdwardGoomba <edward@dock90.io>
2019-06-17 15:56:20 -0400

    Back to square one

--- web/src/routes/about.svelte
+++
@@ -1,7 +0,0 @@
-<svelte:head>
-	<title>About</title>
-</svelte:head>
-
-<h1>About Me: Edward</h1>
-
-<p>This is the 'about' page. There's not much here at the moment. I made the smart choice of deleting my old site without saving copy. So.. ill update this at some point.</p>
